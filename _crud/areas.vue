b37b608f0793c347ed25f88a0c986e997a1099f0
José Alejandro Sabogal Loaiza <jose.alejandro.sabogal.loaiza@gmail.com>
2023-04-17 17:56:08 -0500

    replace apiRoute

--- _crud/areas.vue
+++ _crud/areas.vue
@@ -88,7 +88,7 @@ export default {
               color:"primary"
             },
             loadOptions: {
-              apiRoute: 'apiRoutes.qramp.setupStations',
+              apiRoute: 'apiRoutes.qsetupagione.setupStations',
               select: {label: 'stationName', id: 'id'},
             },
           }
